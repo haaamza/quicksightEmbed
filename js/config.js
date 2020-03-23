7c5d3e7a5a28ce249cc139c0793de1d98e9a426e
Haaamza <hamzagee@outlook.com.com>
2020-03-23 16:19:59 +0500

    first commit

---
+++ js/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_U97K6bDoC', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '4t4g9d6c4kakd94uospv78pakk' //is this used anywhere?
+    },
+};
+
